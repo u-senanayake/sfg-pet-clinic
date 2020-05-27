7db0ff98851f51f57ae1ad53790c6dd509b98961
udayanga91 <u.senanayake91@gmail.com>
2020-05-27 11:20:49 +0900

    Implemented Repositories. Closes #39

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/SpecialityRepository.java
@@ -0,0 +1,7 @@
+package guru.springframework.sfgpetclinic.repositories;
+
+import guru.springframework.sfgpetclinic.model.Specialty;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SpecialityRepository extends CrudRepository<Specialty, Long> {
+}
