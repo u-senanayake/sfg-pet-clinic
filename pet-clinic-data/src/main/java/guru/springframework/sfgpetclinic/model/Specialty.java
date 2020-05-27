502eac786fee14c14e90183b1d664e892051c1ec
udayanga91 <u.senanayake91@gmail.com>
2020-05-27 16:09:43 +0900

    Implemented JPS visit service added loader data. Closes #47

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Specialty.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Specialty.java
@@ -5,7 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "spetialities")
+@Table(name = "specialities")
 public class Specialty extends BaseEntity{
 
     @Column(name = "description")
