87d4de82e4861db76ecfbe34d9d39b7b6f41f9ee
udayanga91 <u.senanayake91@gmail.com>
2020-05-28 18:43:36 +0900

    Upgraded to JUnit 5. Closes #4

--- pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTest.java
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTest.java
@@ -1,10 +1,20 @@
 package guru.springframework.sfgpetclinic;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
 class SfgPetClinicApplicationTest {
 
     @Test
     void main() {
+    }
+
+    @Test
+    public void contextLoads(){
+
     }
 }
