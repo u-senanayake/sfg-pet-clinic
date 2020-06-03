70ce7b51dc1f7d6d3e9eb1d7db3b7358b1c32e55
udayanga91 <u.senanayake91@gmail.com>
2020-06-03 18:48:15 +0900

    Fixed parse issue on PetType. Closes #73

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/formatter/PetTypeFormatter.java
@@ -0,0 +1,37 @@
+package guru.springframework.sfgpetclinic.formatter;
+
+import guru.springframework.sfgpetclinic.model.PetType;
+import guru.springframework.sfgpetclinic.services.PetTypeService;
+import org.springframework.format.Formatter;
+import org.springframework.stereotype.Component;
+
+import java.text.ParseException;
+import java.util.Collection;
+import java.util.Locale;
+
+@Component
+public class PetTypeFormatter implements Formatter<PetType> {
+
+    private final PetTypeService petTypeService;
+
+    public PetTypeFormatter(PetTypeService petTypeService) {
+        this.petTypeService = petTypeService;
+    }
+
+    @Override
+    public String print(PetType petType, Locale locale) {
+        return petType.getName();
+    }
+
+    @Override
+    public PetType parse(String s, Locale locale) throws ParseException {
+        Collection<PetType> findPetType = petTypeService.findAll();
+
+        for (PetType type : findPetType) {
+            if (type.getName().equals(s)) {
+                return type;
+            }
+        }
+        throw new ParseException("Type not found " + s, 0);
+    }
+}
