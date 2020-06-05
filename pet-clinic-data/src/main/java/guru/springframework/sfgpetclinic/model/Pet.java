f93a37f17524321f15d5bc74c3873fb965d1330b
udayanga91 <u.senanayake91@gmail.com>
2020-06-05 13:08:27 +0900

    Fixed pet visit Date validation falling. Closes #74

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -1,6 +1,7 @@
 package guru.springframework.sfgpetclinic.model;
 
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import java.time.LocalDate;
@@ -40,6 +41,7 @@ public class Pet extends BaseEntity {
     private Owner owner;
 
     @Column(name = "birth_day")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthDay;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
